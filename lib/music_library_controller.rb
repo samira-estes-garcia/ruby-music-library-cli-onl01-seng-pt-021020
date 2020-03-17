5fdbe681092092d8679d360755a1124695c6039b
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-17 23:10:48 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -44,6 +44,7 @@ class MusicLibraryController
   def list_artists
     Artist.all.sort{ |a,b| a.name <=> b.name}.each.with_index(1) do |artist, index| puts "#{index}. #{artist.name}"
   end
+  end
   
   
   
