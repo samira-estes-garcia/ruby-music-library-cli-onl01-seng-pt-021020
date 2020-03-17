19cda6640bf7e64f49e10e8dee9b019be716d6b9
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-17 22:45:46 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,6 +18,9 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
+      
+      
+      
     end
     
     input = gets.strip
