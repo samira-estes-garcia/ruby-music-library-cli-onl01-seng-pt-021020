14a6550e977df3e0cd89b124607292a4cc2b914f
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-17 23:09:51 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -42,7 +42,7 @@ class MusicLibraryController
   end
   
   def list_artists
-    
+    Song.all.sort{ |a,b| a.name <=> b.name}.each.with_index(1) do |song, index| puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
   end
   
   
